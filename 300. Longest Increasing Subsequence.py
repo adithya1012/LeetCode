a3099df6911c4f0b52676404ad27f7dffd7a60f4
adithya1012 <seesa01@pfw.edu>
2025-10-25 16:05:48 -0700

    new problem

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -1,15 +1,18 @@
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        self.res = 0
-        stack = []
-        def dfs(i):
-            if i >= len(nums):
-                self.res = max(len(stack), self.res)
-                return
-            if not stack or stack[-1] > nums[i]:
-                stack.append(nums[i])
-                dfs(i+1)
-                stack.pop()
+
+def lengthOfLIS(nums) -> int:
+    res = 0
+    stack = []
+    def dfs(i):
+        if i >= len(nums):
+            nonlocal res
+            res = max(len(stack), res)
+            return
+        if not stack or stack[-1] < nums[i]:
+            stack.append(nums[i])
             dfs(i+1)
-        dfs(0)
-        return self.res
+            stack.pop()
+        dfs(i+1)
+    dfs(0)
+    return res
+
+lengthOfLIS([0,1,0,3,2,3])
