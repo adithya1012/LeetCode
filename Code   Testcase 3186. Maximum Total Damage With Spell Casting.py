0bcab7d1b076cccdb8cc3a8de97d0a6f38b4921e
adithya1012 <seesa01@pfw.edu>
2025-11-02 21:23:48 -0800

    new problems

--- Code   Testcase 3186. Maximum Total Damage With Spell Casting.py
+++ Code   Testcase 3186. Maximum Total Damage With Spell Casting.py
@@ -1,19 +1,23 @@
-class Solution:
-    def maximumTotalDamage(self, power: List[int]) -> int:
-        count = Counter(power)
-        count_list = [(key, key*val) for key, val in count.items()]
-        count_list.sort()
-        dp = {}
-        def dfs(i):
-            if i >= len(count_list):
-                return 0
-            if i in dp:
-                return dp[i]
-            consider = 0
-            for  j in range(i+1, len(count_list)):
-                if count_list[i][0]  < count_list[j][0]-2:
-                    consider = max(consider, count_list[i][1]+dfs(j))
-            skip = dfs(i+1)
-            dp[i] = max(skip, consider)
-            return max(skip, consider)
-        return dfs(0)
+import collections
+
+
+def maximumTotalDamage(power) -> int:
+    count = collections.Counter(power)
+    count_list = [(key, key*val) for key, val in count.items()]
+    count_list.sort()
+    dp = {}
+    def dfs(i):
+        if i >= len(count_list):
+            return 0
+        if i in dp:
+            return dp[i]
+        consider = count_list[i][1]
+        for  j in range(i+1, len(count_list)):
+            if count_list[i][0] < count_list[j][0]-2:
+                consider = max(consider, count_list[i][1]+dfs(j))
+        skip = dfs(i+1)
+        dp[i] = max(skip, consider)
+        return max(skip, consider)
+    return dfs(0)
+
+print(maximumTotalDamage([1,1,3,4]))
